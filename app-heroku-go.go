fcd5f3de3bf5c6616630ae2c785d95312ed2823f
yla@zwenex.com <ye-lin-aung>
2016-04-27 15:13:00 +0630

    Added Mux

--- app-heroku-go.go
+++ app-heroku-go.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 	"os"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, "\n\tHello World From Golang on Heroku")
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
 
 	fmt.Println("Server Listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), r)
 	if err != nil {
 		panic("ListenAndServe")
 	}
