4fb8174b2e9daa11fa3156ea7feafa4fc14a8cd2
yla@zwenex.com <ye-lin-aung>
2016-04-27 15:21:00 +0630

    Added Mux Test Api

--- app-heroku-go.go
+++ app-heroku-go.go
@@ -12,10 +12,15 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "\n\tHello World From Golang on Heroku")
 }
 
+func apiTest(w http.ResponseWriter, r *http.Request) {
+
+	v := mux.Vars(r)
+	fmt.Fprintf(w, "\n\t Hello%s", v["id"])
+}
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
-
+	r.HandleFunc("/a/{id}", apiTest)
 	fmt.Println("Server Listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), r)
 	if err != nil {
