e9cc043ee2006a83935ce084086b435845796743
yla@zwenex.com <ye-lin-aung>
2016-04-27 15:42:39 +0630

    Added Postgres

--- app-heroku-go.go
+++ app-heroku-go.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"log"
 	"net/http"
 	"os"
+)
+
+var (
+	repeat int
+	db     *sql.DB = nil
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,13 +25,29 @@ func apiTest(w http.ResponseWriter, r *http.Request) {
 	v := mux.Vars(r)
 	fmt.Fprintf(w, "\n\t Hello%s", v["id"])
 }
+
+func db(w http.ResponseWriter, r *http.Request) {
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+		fmt.Sprintf("Error creating database table: %q", err)
+		return
+	}
+
+	r.redirect(w, r, "/", nil)
+}
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/db", db)
 	r.HandleFunc("/a/{id}", apiTest)
 	fmt.Println("Server Listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), r)
 	if err != nil {
 		panic("ListenAndServe")
 	}
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+
 }
